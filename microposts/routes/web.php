844e4a491be1661c87ffa0cddebdff6750f60632
hogehoge.tarou <hogehogerakumma@gmail.com>
2018-06-13 07:20:15 +0000

    post

--- microposts/routes/web.php
+++ microposts/routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function() {
-    return view('welcome');
-});
+Route::get('/', 'MicropostsController@index');
 
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
@@ -22,4 +20,5 @@ Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('sign
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::group(['middleware' => ['auth']], function(){
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
